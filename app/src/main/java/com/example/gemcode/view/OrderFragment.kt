f32c4364a3019b9e75f7cfd1f23eb5427c4222e5
lsg1024 <zks145@naver.com>
2023-08-15 01:59:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gemcode/view/OrderFragment.kt
@@ -0,0 +1,33 @@
+package com.example.gemcode.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gemcode.R
+import com.example.gemcode.viewmodel.OrderViewModel
+
+class OrderFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = OrderFragment()
+    }
+
+    private lateinit var viewModel: OrderViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_order, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
